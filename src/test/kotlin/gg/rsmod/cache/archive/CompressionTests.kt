b849e781a084f1b707e2b8e144e1fe8feec472dc
Tom0017 <rspsmods@gmail.com>
2019-08-08 19:45:30 -0400

    Fix line consistency

--- src/test/kotlin/gg/rsmod/cache/archive/CompressionTests.kt
+++ src/test/kotlin/gg/rsmod/cache/archive/CompressionTests.kt
@@ -138,6 +138,7 @@ class CompressionTests {
     }
 
     companion object {
+
         private val data = byteArrayOf(
             0, 8, 16, 24, 32, 40, 48, 56, 64,
             72, 80, 88, 96, 104, 112, 120
