39591c00c2864b9515c833fe7e6aa6225ac5ddd2
Tom0017 <rspsmods@gmail.com>
2019-08-08 18:37:14 -0400

    Fix xtea test

--- src/test/kotlin/gg/rsmod/cache/util/XteaTests.kt
+++ src/test/kotlin/gg/rsmod/cache/util/XteaTests.kt
@@ -9,22 +9,23 @@ class XteaTests {
 
     @Test
     fun `cipher data`() {
-        val dataWriter = WriteOnlyPacket(capacity = Int.SIZE_BYTES * 10)
-        for (i in 0 until dataWriter.array.size / Int.SIZE_BYTES) {
-            dataWriter.p4(i * 8)
-        }
-
-        val data = dataWriter.array
         val enciphered = Xtea.encipher(data, 0, data.size, key)
 
-        val decipherIncorrectKey = Xtea.decipher(enciphered, intArrayOf(1, 2, 4, 3))
+        val decipherIncorrectKey = Xtea.decipher(enciphered.copyOf(), intArrayOf(1, 2, 4, 3))
         assert(!decipherIncorrectKey.contentEquals(data))
 
-        val decipherCorrectKey = Xtea.decipher(enciphered, key)
+        val decipherCorrectKey = Xtea.decipher(enciphered.copyOf(), key)
         assert(decipherCorrectKey.contentEquals(data))
     }
 
     companion object {
+
+        private val data = WriteOnlyPacket(capacity = Int.SIZE_BYTES * 10).apply {
+            for (i in 0 until capacity / Int.SIZE_BYTES) {
+                p4(i * 8)
+            }
+        }.array
+
         private val key = intArrayOf(
             1, 2, 3, 4
         )
